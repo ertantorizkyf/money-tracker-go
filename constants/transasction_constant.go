11f0837cd462d4ef4723795373d0e42c6b2102b3
Rizky Ertanto <ertantorizkyf@gmail.com>
2025-05-10 23:14:52 +0700

    Update sequence documentations and trx constant

--- constants/transasction_constant.go
+++ constants/transasction_constant.go
@@ -4,3 +4,8 @@ const (
 	TRANSACTION_TYPE_EXPENSE string = "expense"
 	TRANSACTION_TYPE_INCOME  string = "income"
 )
+
+const (
+	TRANSACTION_ORDER_NEWEST string = "newest"
+	TRANSACTION_ORDER_OLDEST string = "oldest"
+)
